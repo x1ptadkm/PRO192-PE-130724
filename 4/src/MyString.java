4a89a6bba2d73a6cce2f1947428a1a6cdc350887
x1ptadkm <x1ptadkm@gmail.com>
2025-03-28 22:54:33 +0700

    update

---
+++ 4/src/MyString.java
@@ -0,0 +1,12 @@
+public class MyString implements IString{
+
+    @Override
+    public int f1(String string) {
+        return 0;
+    }
+
+    @Override
+    public String f2(String string) {
+        return null;
+    }
+}
